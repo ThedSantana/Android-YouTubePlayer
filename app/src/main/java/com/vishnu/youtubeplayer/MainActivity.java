894da4211895ebeb7e19d6acf3126fe7a5c4fec4
Vishnu Sosale <sosalevishnu@gmail.com>
2016-01-19 16:29:49 +0530

    Modify Google API key, Add log

--- app/src/main/java/com/vishnu/youtubeplayer/MainActivity.java
+++ app/src/main/java/com/vishnu/youtubeplayer/MainActivity.java
@@ -1,9 +1,18 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Vishnu Sosale
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.vishnu.youtubeplayer;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.util.Log;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -12,8 +21,9 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
-    public static final String GOOGLE_API_KEY = "AIzaSyA8zCox4Qe_PSzVZUzP1qycZrhzz4zL3DA";
+    public static final String GOOGLE_API_KEY = "AIzaSyCKmgMxrGqt81lpdgrObSZLb8DJZFAE_DU`";
     public static final String YOUTUBE_VIDEO_ID = "y6Sxv-sUYtM";
+    final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +33,13 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
         YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtubePlayerView);
         youTubePlayerView.initialize(GOOGLE_API_KEY, this);
 
-
     }
-
-
 
     @Override
     public void onInitializationSuccess
             (YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+
+        Log.e(TAG, "Initialized youtube player");
 
         youTubePlayer.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
             @Override
@@ -91,8 +100,7 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
             }
         });
 
-        if (!wasRestored)
-        {
+        if (!wasRestored) {
             youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
         }
 
@@ -102,7 +110,6 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
     public void onInitializationFailure
             (YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
 
-        Toast.makeText(this, "Cannot initialise", Toast.LENGTH_LONG).show();
-
+        Log.e(TAG, "Failed to initialize youtube player");
     }
 }
